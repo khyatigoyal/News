00c45a85857ce454f4a4695a29cbe4fc9bc612ec
Khyati Goyal <17uec065@lnmiit.ac.in>
2019-07-11 19:04:27 +0530

    updated design

--- src/components/categorymenu.js
+++ src/components/categorymenu.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import Entertainment from '../images/entertainment.png';
+import Science from '../images/science.png';
+import Technology from '../images/technology.png';
+import Health from '../images/health.png';
+import Business from '../images/business.png';
+import Sports from '../images/sports.png';
+import General from '../images/general.png';
 class CategoryMenu extends React.Component{
     handleclick =(category)=>{
         this.props.handleClick(category)
@@ -7,29 +14,29 @@ class CategoryMenu extends React.Component{
 
     return(
             <div className="ui container">
-                <div className="ui inverted fixed top seven item massive menu">
+                <div className="ui inverted fixed top seven item large menu">
             
                         <a onClick={()=>{this.handleclick("entertainment")}} className="item">
-                        <i className="film large icon"/>
+                        <img alt="..." src={Entertainment} className="avatar rounded image"/>
                         </a>
                         <a onClick={()=>{this.handleclick("general")}} className="item">
-                        <i className="file large icon"></i>
+                        <img alt="..." src={General} className="avatar rounded image"/>
                         </a>
                         <a onClick={()=>{this.handleclick("sports")}} className="item">
-                        <i className="football ball large icon"></i>
+                        <img alt="..." src={Sports} className="avatar rounded image"/>
                         
                         </a>
                         <a onClick={()=>{this.handleclick("science")}} className="item">
-                        <i className="user md large icon"/>
+                        <img alt="..." src={Science} className="avatar rounded image"/>
                         </a>
                         <a onClick={()=>{this.handleclick("technology")}} className="item">
-                        <i className="laptop large icon"/>
+                        <img alt="..." src={Technology} className="avatar rounded image"/>
                         </a>
                         <a onClick={()=>{this.handleclick("health")}} className="item">
-                        <i className="medkit large icon"></i>
+                        <img alt="..." src={Health} className="avatar rounded image"/>
                         </a>
                         <a onClick={()=>{this.handleclick("business")}} className="item">
-                        <i className="chart line large icon"></i>
+                        <img alt="..." src={Business} className="avatar rounded image"/>
                         </a>
                         </div>         
                     </div>
