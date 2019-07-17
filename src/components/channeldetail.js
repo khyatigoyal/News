4a560dec04512a3a81d074d3e81661dcceef9056
Khyati Goyal <17uec065@lnmiit.ac.in>
2019-07-17 19:50:10 +0530

    designed news site

--- src/components/channeldetail.js
+++ src/components/channeldetail.js
@@ -91,41 +91,5 @@ class channelDetail extends React.Component{
         )
     }
 }
-/*const  countryflag = channel.country + " flag";
-    return(
-        
-        <div className="ui raised centered card">
-  <div className="ui slide masked reveal image">
-    <img alt="Loading..." src={GirlReporter} className="hidden content" />
-    <img alt="Loading..." src={MaleReporter} className="visible content" />
-  </div>
-  <div className="content">
-  <i className="right floated like icon"></i>
-    <i className="right floated star icon"></i>
-    <a href={channel.url} className="header">{channel.name}</a>
-    <div className="meta">
-      <span className="date">{channel.id}</span>
-    </div>
-    <div className="description">
-      {channel.description}
-    </div>
-  </div>
-  <div className="extra content">
-    <div className="item">
-        <a href={channel.url}>
-      <i className="paper plane icon"></i>
-      {channel.url}
-    </a></div>
-    <div className="item">
-        <i className={countryflag}></i>
-        {channel.country}
-    </div>
-    <div className="item">
-        <img alt="..." className="ui mini image"src={Language} />
-        {channel.language}
-    </div>
-  </div>
-</div>
 
-    )*/
 export default channelDetail;
