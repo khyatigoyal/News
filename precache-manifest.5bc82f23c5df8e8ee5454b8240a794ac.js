606f9064ab4b71414b068f7266b6068b75377f30
Khyati Goyal <17uec065@lnmiit.ac.in>
2019-07-17 20:24:51 +0530

    Updates

---
+++ precache-manifest.5bc82f23c5df8e8ee5454b8240a794ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff44273d0855fd8eee5b76336be1d393",
+    "url": "/News/index.html"
+  },
+  {
+    "revision": "0639a666c1849c70bb6d",
+    "url": "/News/static/js/2.af04cce6.chunk.js"
+  },
+  {
+    "revision": "fdc2b445a09150bd9123",
+    "url": "/News/static/js/main.a6ed241b.chunk.js"
+  },
+  {
+    "revision": "d37a9a776d21fd59755b",
+    "url": "/News/static/js/runtime~main.a19ba134.js"
+  },
+  {
+    "revision": "48e5095a565a37c853fb07ade0e27f6c",
+    "url": "/News/static/media/business.48e5095a.png"
+  },
+  {
+    "revision": "1f6a54902ee5438e6818ed4d9ef64f9b",
+    "url": "/News/static/media/health.1f6a5490.png"
+  },
+  {
+    "revision": "06605b7c9c7d6266159973b0e3159dd3",
+    "url": "/News/static/media/malereporter.06605b7c.png"
+  },
+  {
+    "revision": "5ee4bd1df0620529167dd86e5fa7f2a4",
+    "url": "/News/static/media/pngtree-tv-head.5ee4bd1d.png"
+  },
+  {
+    "revision": "c38d983ea54e5c45cf95e83dc579674a",
+    "url": "/News/static/media/reporter.c38d983e.png"
+  },
+  {
+    "revision": "e78057e535f418e48e48f5c87e5a7d7c",
+    "url": "/News/static/media/snippets.e78057e5.png"
+  },
+  {
+    "revision": "2afceed3fd61f69e815a8eb9b30fc362",
+    "url": "/News/static/media/sports.2afceed3.png"
+  },
+  {
+    "revision": "aa0cd47fa876de46ae1c3f2dd0a568e4",
+    "url": "/News/static/media/technology.aa0cd47f.png"
+  }
+]);
